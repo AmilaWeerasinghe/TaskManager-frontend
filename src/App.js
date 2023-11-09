1746e75dcf6b0cd58b443f43e007d9ed4c1aa57c
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2023-11-09 21:07:05 +0530

    update to use backend apis

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dashboard from './components/Dashboard';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    // Fetch tasks from the backend when the component mounts
+    fetch('http://localhost:5001/tasks')
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.error('Error fetching tasks:', error));
+  }, []);
+
   const handleAddTask = (newTask) => {
-    setTasks([...tasks, { id: tasks.length + 1, ...newTask }]);
+    fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // Refresh the tasks after adding a new task
+        fetch('http://localhost:5000/tasks')
+          .then((response) => response.json())
+          .then((data) => setTasks(data))
+          .catch((error) => console.error('Error fetching tasks:', error));
+      })
+      .catch((error) => console.error('Error adding task:', error));
   };
 
   const handleDeleteTask = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
+    fetch(`http://localhost:5000/tasks/${taskId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // Refresh the tasks after deleting a task
+        fetch('http://localhost:5000/tasks')
+          .then((response) => response.json())
+          .then((data) => setTasks(data))
+          .catch((error) => console.error('Error fetching tasks:', error));
+      })
+      .catch((error) => console.error('Error deleting task:', error));
   };
 
   return (
